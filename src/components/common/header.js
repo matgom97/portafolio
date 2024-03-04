e7c9dd8baacc4130d48af9fcc5339f24e9aa49e9
matgom97 <93955069+matgom97@users.noreply.github.com>
2024-03-04 18:52:39 -0500

    create scroll hidden navbar

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -4,21 +4,21 @@ import styles from '../styles/main.module.scss';
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [visible, setVisible] = useState(true);
-
+  const [cliced, setClicked] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
-      // Verificar si el usuario ha hecho scroll manualmente
-      if (window.pageYOffset !== 0 && menuOpen === false) {
-        setVisible(false);
-      } else {
+      
+      if (window.pageYOffset >= 0 && cliced === false) {
         setVisible(true);
       }
     };
-
+  
     window.addEventListener('scroll', handleScroll);
-
+  
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [menuOpen]); // Agregamos menuOpen a la lista de dependencias
+  }, [cliced, scrolling, visible]); // Observamos 'cliced' y 'scrolling' en el arreglo de dependencias
+  
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -30,17 +30,25 @@ const Header = () => {
       section.scrollIntoView({ behavior: 'smooth' });
       setMenuOpen(false);
       setVisible(false);
+      setScrolling (true)
     }
+  };
+
+  // Manejador de evento para clics en enlaces
+  const handleClick = () => {
+    console.log("¡Se hizo clic en un enlace!");
+    setClicked(true);
+    setTimeout(() => setClicked(false), 900);
   };
 
   return (
     <header className={`${styles.header} ${visible ? styles.show : styles.hide}`}>
       <nav className={styles.menu}>
         <div className={styles.menuItems}>
-          <a onClick={() => scrollToSection('init')}>Inicio</a>
-          <a onClick={() => scrollToSection('skills')}>Habilidades</a>
-          <a onClick={() => scrollToSection('experience')}>Experiencia</a>
-          <a onClick={() => scrollToSection('education')}>Educación</a>
+          <a onClick={() => { scrollToSection('init'); handleClick(); }}>Inicio</a>
+          <a onClick={() => { scrollToSection('skills'); handleClick(); }}>Habilidades</a>
+          <a onClick={() => { scrollToSection('experience'); handleClick(); }}>Experiencia</a>
+          <a onClick={() => { scrollToSection('education'); handleClick(); }}>Educación</a>
         </div>
         <div>
           <button onClick={toggleMenu} className={styles.menuButton}>
@@ -55,10 +63,10 @@ const Header = () => {
 
       <div className={`${styles.menuOverlay} ${menuOpen ? styles.menuOpen : ''}`}>
         <div className={styles.menuContent}>
-          <a onClick={() => { scrollToSection('init'); }}>Inicio</a>
-          <a onClick={() => { scrollToSection('skills'); }}>Habilidades</a>
-          <a onClick={() => { scrollToSection('experience'); }}>Experiencia</a>
-          <a onClick={() => { scrollToSection('education'); }}>Educación</a>
+          <a onClick={() => { handleClick(); scrollToSection('init'); }}>Inicio</a>
+          <a onClick={() => { handleClick(); scrollToSection('skills'); }}>Habilidades</a>
+          <a onClick={() => { handleClick(); scrollToSection('experience'); }}>Experiencia</a>
+          <a onClick={() => { handleClick(); scrollToSection('education'); }}>Educación</a>
           <a onClick={toggleMenu}>Cerrar</a>
         </div>
       </div>
