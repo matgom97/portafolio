ff38a6c344b7c0bf03366c7b8d56d0d28eeb721a
matgom97 <93955069+matgom97@users.noreply.github.com>
2024-03-07 22:02:29 -0500

    add first part of portafolio it is a concept

--- src/components/home/presentation.js
+++ src/components/home/presentation.js
@@ -2,19 +2,34 @@
 import styles from '../styles/main.module.scss';
 
 const Presentation = () => {
-  
+
 
   return (
     <div className={styles.initial} id='init'>
-        <div>
-            <h1>
-                Hola! soy Mateo Gomez Osio.
-                Ingeniero de sistemas y programador
-            </h1>
+      <div className={styles.programming_environment}>
+        <div className={styles.terminal}>
+          <div className={styles.terminal_header}>
+            <span className={styles.terminal_title}>user@hostname</span>
+            <span className={styles.terminal_path}>~/Documents</span>
+          </div>
+          <div className={styles.terminal_content}>
+            <div className={styles.terminal_input}>
+              <span className={styles.terminal_prompt}>$</span>
+              <span className={styles.terminal_command}>ls</span>
+            </div>
+            <div className={styles.terminal_output}>
+              <p>Desktop</p>
+              <p>Documents</p>
+              <p>Downloads</p>
+              <p>Music</p>
+              <p>Pictures</p>
+              <p>Public</p>
+              <p>Templates</p>
+              <p>Videos</p>
+            </div>
+          </div>
         </div>
-        <div>
-
-        </div>
+      </div>
     </div>
   );
 };
