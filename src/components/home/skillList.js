f9dc6796e208a77632b0a7df7230d12e03098687
matgom97 <93955069+matgom97@users.noreply.github.com>
2024-08-26 18:49:27 -0500

    feat: create skills section

--- src/components/home/skillList.js
+++ src/components/home/skillList.js
@@ -1,22 +1,35 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from '../styles/main.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/main.module.scss";
 
 const SkillList = () => {
   return (
-    
     <section className={styles.fullscreen} id="skills">
-      <h2>Habilidades Técnicas</h2>
-      <ul>
-        <li>JavaScript</li>
-        <li>React.js</li>
-        <li>Node.js</li>
-        {/* Agrega más habilidades según sea necesario */}
-      </ul>
-      <Link to="/skills">Ver Todas las Habilidades</Link>
+      <div className={styles.skills}>
+        <div className={styles.skills}>
+          <div class={styles.card}>
+            <div class={styles.cardContent}>
+              Frontend
+              <ul>
+                <li>JavaScript</li>
+                <li>React.js</li>
+                <li>Node.js</li>
+                <li>lara</li>
+              </ul>
+            </div>
+            <div class={styles.divider}></div>
+            <div class={styles.cardContent}>
+              Backend
+              <ul>
+                <li>laravel</li>
+                <li>.Net Core</li>
+                <li>SQL</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-    
   );
 };
 
